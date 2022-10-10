897f18b80eaec67f304c8444e63378fd4d171826
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-10-10 12:50:56 +0530

    "movie data"

--- hulu-app/components/Nav.js
+++ hulu-app/components/Nav.js
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from "react";
 import requests from "../utils/requests";
-import axios from "axios"
+import axios from "axios";
 // http://www.omdbapi.com/?t=Game of Thrones&Season=1
 
 const Nav = () => {
-  const [Moviedata,setMoviedata] = useState([])
-useEffect(()=>{
-  axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=43863da1&type=movies`).then((response)=>{
-    console.log(response)
-    setMoviedata(response.data)
-   }).catch((err)=>{
-    console.log(err)
-    return err
-   })
-},[])
+  const [Moviedata, setMoviedata] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://www.omdbapi.com/?i=tt3896198&apikey=43863da1&s=Batman`)
+      .then((response) => {
+        console.log(response);
+        setMoviedata(response.data.Search);
+      })
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+  }, []);
 
-   console.log("Moviedata",Moviedata)
+  console.log("Moviedata", Moviedata);
 
   return (
     <nav>
       <div className="">
-        <img src={Moviedata.Poster}/>
-     <h2> {Moviedata.Title}</h2>
+        {Moviedata.map((e, index) => (
+          <>
+            <img src={e.Poster} alt={e.Title}/>
+            <div>{e.Title}</div>
+            <div>{e.Year}</div>
+            <div>{e.Type}</div>
+          </>
+        ))}
 
-        {/* {Object.entries(requests).map(([key, { title, url }]) => {
-          // `console.log("title",title)`
-          <h2
-            key={key}
-            className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
-          >
-            {title}
-          </h2>;
-        })} */}
       </div>
     </nav>
   );
